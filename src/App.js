f829951b78c4b192546a8b03072888afa21a3dcb
Pulkit Ujjainwal <pulkitujjainwal66@gmail.com>
2023-07-25 20:10:01 +0530

    Button Developed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Layout, Menu } from 'antd';
+import { DesktopOutlined } from '@ant-design/icons';
+import { BrowserRouter as Router, Route, Link, Switch, Redirect } from 'react-router-dom';
 import './App.css';
+import TransactionReportPage from './TransactionReportPage';
 
-function App() {
+const { Sider, Header, Content } = Layout;
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Layout style={{ minHeight: '100vh' }}>
+        <Sider width={200} theme="dark">
+          <Menu mode="inline" defaultSelectedKeys={['1']} style={{ height: '100%', borderRight: 0 }}>
+            <Menu.Item key="1" icon={<DesktopOutlined />}>
+              <Link to="/transactions">Transaction Report</Link>
+            </Menu.Item>
+          </Menu>
+        </Sider>
+        <Layout>
+          <Header style={{ background: '#fff', padding: '0 16px' }}>
+            <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'flex-end', height: '100%' }}>
+              <h1>Policy Data Downloader</h1>
+            </div>
+          </Header>
+          <Content style={{ margin: '24px 16px 0' }}>
+            <div style={{ padding: 24, minHeight: 360 }}>
+              <Switch>
+                <Route path="/transactions">
+                  <TransactionReportPage />
+                </Route>
+                <Redirect to="/transactions" />
+              </Switch>
+            </div>
+          </Content>
+        </Layout>
+      </Layout>
+    </Router>
   );
-}
+};
 
 export default App;
---
+++ src/TransactionReportPage.js
@@ -0,0 +1,128 @@
+import React from 'react';
+import { Table, Button } from 'antd';
+import './TransactionReportPage.css';
+
+const TransactionReportPage = () => {
+  // Dummy data (Replace with your actual policy data)
+  const data = [
+    {
+      product_name: 'Product A',
+      name: 'John Doe',
+      phone_number: '1234567890',
+      email: 'john.doe@example.com',
+      premium_paid: '$2000',
+      insurer: 'Insurer A',
+      sum_assured: '$50,000',
+      date_of_insurance: '2022-01-01',
+      date_of_expiry: '2023-01-01',
+      policy_number: 'POL-001',
+    },
+    {
+      product_name: 'Product B',
+      name: 'Jane Smith',
+      phone_number: '9876543210',
+      email: 'jane.smith@example.com',
+      premium_paid: '$1800',
+      insurer: 'Insurer B',
+      sum_assured: '$40,000',
+      date_of_insurance: '2022-02-01',
+      date_of_expiry: '2023-02-01',
+      policy_number: 'POL-002',
+    },
+    // Add more dummy data...
+  ];
+
+  const columns = [
+    {
+      title: 'Product Name',
+      dataIndex: 'product_name',
+      key: 'product_name',
+    },
+    {
+      title: 'Name',
+      dataIndex: 'name',
+      key: 'name',
+    },
+    {
+      title: 'Phone Number',
+      dataIndex: 'phone_number',
+      key: 'phone_number',
+    },
+    {
+      title: 'Email ID',
+      dataIndex: 'email',
+      key: 'email',
+    },
+    {
+      title: 'Premium Paid',
+      dataIndex: 'premium_paid',
+      key: 'premium_paid',
+    },
+    {
+      title: 'Insurer',
+      dataIndex: 'insurer',
+      key: 'insurer',
+    },
+    {
+      title: 'Sum Assured',
+      dataIndex: 'sum_assured',
+      key: 'sum_assured',
+    },
+    {
+      title: 'Date of Insurance',
+      dataIndex: 'date_of_insurance',
+      key: 'date_of_insurance',
+    },
+    {
+      title: 'Date of Expiry',
+      dataIndex: 'date_of_expiry',
+      key: 'date_of_expiry',
+    },
+    {
+      title: 'Policy Number',
+      dataIndex: 'policy_number',
+      key: 'policy_number',
+    },
+  ];
+
+  // Function to download policy data as a JSON file
+  const downloadPolicyData = () => {
+    // Convert data to JSON string
+    const jsonData = JSON.stringify(data, null, 2);
+
+    // Create a Blob from the JSON data
+    const blob = new Blob([jsonData], { type: 'application/json' });
+
+    // Create a temporary link and click it to trigger the download
+    const url = URL.createObjectURL(blob);
+    const link = document.createElement('a');
+    link.href = url;
+    link.download = 'policy_data.json';
+    document.body.appendChild(link);
+    link.click();
+
+    // Clean up the temporary link
+    URL.revokeObjectURL(url);
+    document.body.removeChild(link);
+  };
+
+  return (
+    <div className="transaction-report-page">
+      <h1 className="transaction-report-page-heading">Transaction Report</h1>
+
+      <Table
+        className="transaction-report-table"
+        dataSource={data}
+        columns={columns}
+        rowKey="policy_number"
+        pagination={{ pageSize: 10 }}
+      />
+
+      <Button type="primary" onClick={downloadPolicyData}>
+        Download Policy Data (JSON)
+      </Button>
+    </div>
+  );
+};
+
+export default TransactionReportPage;
